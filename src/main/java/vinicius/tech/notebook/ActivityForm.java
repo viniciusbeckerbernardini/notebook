797b32689e3b593b46539236dac013e0579abf2f
Lab Android <lab.android@esade.local>
2019-08-19 21:52:23 -0300

    First commit

---
+++ src/main/java/vinicius/tech/notebook/ActivityForm.java
@@ -0,0 +1,14 @@
+package vinicius.tech.notebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
